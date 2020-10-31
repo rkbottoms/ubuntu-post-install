1c02765145bdf7473d49450d0ae70fbf1446ebee
Ryan Bottoms <rkbottoms@users.noreply.github.com>
2020-10-31 11:30:15 -0500

    Added lines to script installation of python tools

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -5,6 +5,8 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
+# install tool to change file endings
+sudo apt-get install -y dos2unix
 
 # install file archiving/extraction tools
 sudo apt-get install -y unace rar unrar p7zip-rar p7zip zip unzip sharutils uudeview mpack lha arj cabextract file-roller
@@ -17,15 +19,25 @@ sudo apt-get install -y git gitk
 git config --global user.name "Ryan Bottoms"
 git config --global user.email "rkbottoms@users.noreply.github.com"
 
+# install python tools
+sudo apt-get install python3 python3-pip
+sudo apt-get install python3-venv
+# VS Code complains if Pylint is unavailable as it cannot do some code checking:
+pip3 install pylint
+pip3 install virtualenvwrapper
+
 # install json tools 
 sudo apt-get install jq
+
 # productivity tools
 sudo apt-get install -y fzf fd-find httpie ripgrep htop
 wget -O- https://bit.ly/glances | /bin/bash
 
 # install dotfiles
+cd ../
 git clone https://github.com/rkbottoms/dotfiles.git
 bash dotfiles/install-dotfiles.sh
+rm -rf /dotfiles
 
 #create folder to projects
 if [ ! -d /mnt/c/Users/ryanb/My Documents/Development ]; 
